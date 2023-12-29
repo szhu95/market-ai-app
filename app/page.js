987abf0e8b7f5cc1007599606f1c6622beb39b5e
Stephen Hu <stephenhu@Stephens-MBP.lan>
2023-12-28 18:48:45 -0800

    removing classname on main page

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/email" className="focus:bg-primary-blue focus:text-white hover:bg-primary-blue hover:text-white">Email Generator Tool</Link>
-      <Link href="/social" className="padding-x focus:bg-primary-blue focus:text-white hover:bg-primary-blue hover:text-white">Social Media Post Generator Tool</Link>
+      <Link href="/email" >Email Generator Tool</Link>
+      <Link href="/social">Social Media Kudos Post Generator Tool</Link>
     </main>
   )
 }
